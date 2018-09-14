be6bc97566cfe7c9d1000ef7366630ecb6bc0737
Guha Balakrishnan <balakg@visiongpu04.csail.mit.edu>
2018-09-14 10:45:08 -0400

    code cleanup

--- src/networks.py
+++ src/networks.py
@@ -1,84 +1,72 @@
 """
-Networks for voxelwarp model
+Networks for voxelmorph model
 """
 
 # third party
-import tensorflow as tf
 from keras.models import Model
 from keras.layers import Conv3D, Activation, Input, UpSampling3D, concatenate
 from keras.layers import LeakyReLU, Reshape, Lambda
 from keras.initializers import RandomNormal
-import keras
-import numpy as np
 
-# local
-from dense_3D_spatial_transformer import Dense3DSpatialTransformer
-import losses
+import sys
+sys.path.append('../ext/neuron')
+import neuron.layers as nrn_layers
 
 
-
-def unet(vol_size, enc_nf, dec_nf, full_size=True):
+def unet(vol_size, enc_nf, dec_nf):
     """
-    unet network for voxelmorph 
+    unet architecture for voxelmorph models presented in the CVPR 2018 paper. You may need
+    to modify this code (e.g., number of layers) to suit your project needs.
 
-    Args:
-        vol_size: volume size. e.g. (256, 256, 256)
-        enc_nf: encoder filters. right now it needs to be to 1x4.
-            e.g. [16,32,32,32]
-            TODO: make this flexible.
-        dec_nf: encoder filters. right now it's forced to be 1x7.
-            e.g. [32,32,32,32,8,8,3]
-            TODO: make this flexible.
-        full_size
-
+    :param vol_size: volume size. e.g. (256, 256, 256)
+    :param enc_nf: list of encoder filters. right now it needs to be 1x4.
+           e.g. [16,32,32,32]
+    :param dec_nf: list of decoder filters. right now it must be 1x6 (like voxelmorph-1) or 1x7 (voxelmorph-2)
+    :return: the keras model
     """
-
     # inputs
     src = Input(shape=vol_size + (1,))
     tgt = Input(shape=vol_size + (1,))
     x_in = concatenate([src, tgt])
 
-    # down-sample path.
-    x0 = myConv(x_in, enc_nf[0], 2)  # 80x96x112
-    x1 = myConv(x0, enc_nf[1], 2)  # 40x48x56
-    x2 = myConv(x1, enc_nf[2], 2)  # 20x24x28
-    x3 = myConv(x2, enc_nf[3], 2)  # 10x12x14
+    # down-sample path (encoder)
+    x_enc = [x_in]
+    for i in range(len(enc_nf)):
+        x_enc.append(conv_block(x_enc[-1], enc_nf[i], 2))
 
-    # up-sample path.
-    x = myConv(x3, dec_nf[0])
+    # up-sample path (decoder)
+    x = conv_block(x_enc[-1], dec_nf[0])
     x = UpSampling3D()(x)
-    x = concatenate([x, x2])
-    x = myConv(x, dec_nf[1])
+    x = concatenate([x, x_enc[-2]])
+    x = conv_block(x, dec_nf[1])
     x = UpSampling3D()(x)
-    x = concatenate([x, x1])
-    x = myConv(x, dec_nf[2])
+    x = concatenate([x, x_enc[-3]])
+    x = conv_block(x, dec_nf[2])
     x = UpSampling3D()(x)
-    x = concatenate([x, x0])
-    x = myConv(x, dec_nf[3])
-    x = myConv(x, dec_nf[4])
+    x = concatenate([x, x_enc[-4]])
+    x = conv_block(x, dec_nf[3])
+    x = conv_block(x, dec_nf[4])
+    x = UpSampling3D()(x)
+    x = concatenate([x, x_enc[0]])
+    x = conv_block(x, dec_nf[5])
 
-    if full_size:
-        x = UpSampling3D()(x)
-        x = concatenate([x, x_in])
-        x = myConv(x, dec_nf[5])
+    # optional convolution at output resolution (used in voxelmorph-2)
+    if len(dec_nf) == 7:
+        x = conv_block(x, dec_nf[6])
 
-        # optional convolution
-        if (len(dec_nf) == 8):
-            x = myConv(x, dec_nf[6])
-
-    # transform the results into a flow.
-    flow = Conv3D(dec_nf[-1], kernel_size=3, padding='same',
+    # transform the results into a flow field.
+    flow = Conv3D(3, kernel_size=3, padding='same',
                   kernel_initializer=RandomNormal(mean=0.0, stddev=1e-5), name='flow')(x)
 
     # warp the source with the flow
-    y = Dense3DSpatialTransformer()([src, flow])
+    y = nrn_layers.SpatialTransformer(interp_method='linear', indexing='xy')([src, flow])
 
     # prepare model
     model = Model(inputs=[src, tgt], outputs=[y, flow])
     return model
 
 
-def myConv(x_in, nf, strides=1):
+def conv_block(x_in, nf, strides=1):
     """
     specific convolution module including convolution followed by leakyrelu
     """
