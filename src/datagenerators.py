e4509001170d4a84255efb41cb2f81726bffef89
adalca <adalca@mit.edu>
2018-10-03 21:51:51 -0400

    adding support for batch_size greater than 1

--- src/datagenerators.py
+++ src/datagenerators.py
@@ -16,20 +16,35 @@ def load_example_by_name(vol_name, seg_name):
     return tuple(return_vals)
 
 
-def example_gen(vol_names, return_segs=False, seg_dir=None):
+def example_gen(vol_names, batch_size=1, return_segs=False, seg_dir=None):
     #idx = 0
-    while(True):
-        idx = np.random.randint(len(vol_names))
-        X = np.load(vol_names[idx])['vol_data']
-        X = np.reshape(X, (1,) + X.shape + (1,))
+    while True:
+        idxes = np.random.randint(len(vol_names), size=batch_size)
 
-        return_vals = [X]
 
-        if(return_segs):
-            name = os.path.basename(vol_names[idx])
-            X_seg = np.load(seg_dir + name[0:-8]+'aseg.npz')['vol_data']
-            X_seg = np.reshape(X_seg, (1,) + X_seg.shape + (1,))
-            return_vals.append(X_seg)
+        X_data = []
+        for idx in idxes:
+            X = np.load(vol_names[idx])['vol_data']
+            X = np.reshape(X, (1,) + X.shape + (1,))
+            X_data += [X]
+
+        if batch_size > 1:
+            return_vals = [np.concatenate(X_data, 0)]
+        else:
+            return_vals = [X_data[0]]
+
+        if return_segs:
+            X_data = []
+            for idx in idxes:
+                name = os.path.basename(vol_names[idx])
+                X_seg = np.load(seg_dir + name[0:-8]+'aseg.npz')['vol_data']
+                X_seg = np.reshape(X_seg, (1,) + X_seg.shape + (1,))
+                X_data += X_seg
+            
+            if batch_size > 1:
+                return_vals.append(np.concatenate(X_data, 0))
+            else:
+                return_vals.append(X_data[0])
 
         # print vol_names[idx] + "," + seg_dir + name[0:-8]+'aseg.npz'
 
