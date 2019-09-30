7a366ea741e70af601fd32cfa36a778e6ff50183
Adrian Dalca <adalca@mit.edu>
2019-09-29 20:33:16 -0400

    layer cleanup

--- voxelmorph/layers.py
+++ voxelmorph/layers.py
@@ -10,91 +10,65 @@ import sys
 # third party
 import numpy as np
 
-# keras imports.
-# TODO: these imports should be cleaner...
+# keras imports
 import keras.backend as K
-from keras.models import Model
-import keras.layers as KL
 from keras.layers import Layer
-from keras.layers import Conv3D, Activation, Input, UpSampling3D, concatenate
-from keras.layers import LeakyReLU, Reshape, Lambda
-from keras.initializers import RandomNormal
-import keras.initializers
 import tensorflow as tf
 
 # import neuron layers, which will be useful for Transforming.
 # TODO: switch to nice local imports...
 sys.path.append('../ext/neuron')
-sys.path.append('../ext/pynd-lib')
-sys.path.append('../ext/pytools-lib')
 import neuron as ne
 
-# other vm functions
-import losses
+
+# some useful layers from Neuron
+from neuron.layers import Resize, Sample, RescaleValues
 
 
-class Sample(Layer):
+class ResizeTransform(Layer):
     """ 
-    Keras Layer: Gaussian sample from [mu, sigma]
+    Resize a transform, which involves resizing the vector field *and* rescaling it.
     """
-
-    def __init__(self, **kwargs):
-        super(Sample, self).__init__(**kwargs)
+    def __init__(self, zoom_factor, interp_method='linear', **kwargs):
+        self.zoom_factor = zoom_factor
+        self.interp_method = interp_method
+        super(ResizeTransform, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        super(Sample, self).build(input_shape)  # Be sure to call this somewhere!
+        if isinstance(input_shape[0], (list, tuple)) and len(input_shape) > 1:
+            raise Exception('ResizeTransform must be called on a list of length 1.')
 
-    def call(self, x):
-        return sample(x)
+        if isinstance(input_shape[0], (list, tuple)):
+            input_shape = input_shape[0]
 
-    def compute_output_shape(self, input_shape):
-        return input_shape[0]
+        super(ResizeTransform, self).build(input_shape)  # Be sure to call this somewhere!
 
+    def call(self, inputs):
+        # check shapes
+        if isinstance(inputs, (list, tuple)):
+            assert len(inputs) == 1, "inputs has to be len 1. found: %d" % len(inputs)
+            trf = inputs[0]
+        else:
+            trf = inputs
 
-class Negate(Layer):
-    """ 
-    Keras Layer: negative of the input
-    """
-
-    def __init__(self, **kwargs):
-        super(Negate, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        super(Negate, self).build(input_shape)  # Be sure to call this somewhere!
-
-    def call(self, x):
-        return -x
-
-    def compute_output_shape(self, input_shape):
-        return input_shape
-
-
-class Rescale(Layer):
-    """ 
-    Keras layer: rescale data by fixed factor
-    """
-
-    def __init__(self, resize, **kwargs):
-        self.resize = resize
-        super(Rescale, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        super(Rescale, self).build(input_shape)  # Be sure to call this somewhere!
-
-    def call(self, x):
-        return x * self.resize 
+        # multiply first to save memory (multiply Tensor in smaller space)
+        trf = trf * self.zoom_factor
+        return ne.utils.resize(trf, self.zoom_factor, self.interp_method)
 
     def compute_output_shape(self, input_shape):
         return input_shape
 
 
-class RescaleDouble(Rescale):
+class DoubleValues(RescaleValues):
     def __init__(self, **kwargs):
         self.resize = 2
-        super(RescaleDouble, self).__init__(self.resize, **kwargs)
+        super(DoubleValues, self).__init__(self.resize, **kwargs)
 
 
 class ResizeDouble(ne.layers.Resize):
+    """
+    double the size of a volume
+    """
     def __init__(self, **kwargs):
         self.zoom_factor = 2
         super(ResizeDouble, self).__init__(self.zoom_factor, **kwargs)
@@ -102,7 +76,11 @@ class ResizeDouble(ne.layers.Resize):
 
 class LocalParamWithInput(Layer):
     """ 
+
+    Update 9/29/2019 - TODO: should try ne.layers.LocalParam() again after update.
+
     The neuron.layers.LocalParam has an issue where _keras_shape gets lost upon calling get_output :(
+
         tried using call() but this requires an input (or i don't know how to fix it)
         the fix was that after the return, for every time that tensor would be used i would need to do something like
         new_vec._keras_shape = old_vec._keras_shape
@@ -112,10 +90,11 @@ class LocalParamWithInput(Layer):
         this doesn't have the _keras_shape issue since we built on the input and use call()
     """
 
-    def __init__(self, shape, my_initializer='RandomNormal', mult=1.0, **kwargs):
+    def __init__(self, shape, initializer='RandomNormal', mult=1.0, **kwargs):
         self.shape=shape
-        self.initializer = my_initializer
+        self.initializer = initializer
         self.biasmult = mult
+        print('LocalParamWithInput: Consider using neuron.layers.LocalParam()')
         super(LocalParamWithInput, self).__init__(**kwargs)
 
     def build(self, input_shape):
@@ -134,18 +113,3 @@ class LocalParamWithInput(Layer):
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], *self.shape)
-
-
-###############################################################################
-# Helper functions
-###############################################################################
-
-def sample(args):
-    """
-    sample from a normal distribution
-    """
-    mu = args[0]
-    log_sigma = args[1]
-    noise = tf.random_normal(tf.shape(mu), 0, 1, dtype=tf.float32)
-    z = mu + tf.exp(log_sigma/2.0) * noise
-    return z
