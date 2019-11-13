288e9b7df5304eb1aedc3535b5988acd32f9e70c
Andrew Hoopes <athoopes1@gmail.com>
2019-11-13 12:40:37 -0500

    cleanup backend imports

--- voxelmorph/__init__.py
+++ voxelmorph/__init__.py
@@ -1,11 +1,20 @@
+# ---- voxelmorph ----
+# unsupervised learning for image registration
+
 from . import utils
 from . import generators
 
+# import backend-dependent submodules
 if utils.get_backend() == 'pytorch':
+    # the pytorch backend can be enabled by setting the
+    # VXM_BACKEND environment var to "pytorch"
     from . import torch_backend as backend
+    from .torch_backend import layers
+    from .torch_backend import networks
+    from .torch_backend import losses
 else:
+    # tensorflow is default backend
     from . import tf_backend as backend
-
-from backend import layers
-from backend import networks
-from backend import losses
+    from .tf_backend import layers
+    from .tf_backend import networks
+    from .tf_backend import losses
