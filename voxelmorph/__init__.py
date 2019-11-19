e61ef7a1e594b196c3db4fd74a2eb79e539ef2cd
Andrew Hoopes <athoopes1@gmail.com>
2019-11-19 13:25:16 -0500

    save backend name and rename subdirs

--- voxelmorph/__init__.py
+++ voxelmorph/__init__.py
@@ -5,16 +5,17 @@ from . import utils
 from . import generators
 
 # import backend-dependent submodules
-if utils.get_backend() == 'pytorch':
-    # the pytorch backend can be enabled by setting the
-    # VXM_BACKEND environment var to "pytorch"
-    from . import torch_backend as backend
-    from .torch_backend import layers
-    from .torch_backend import networks
-    from .torch_backend import losses
+backend = utils.get_backend()
+if backend == 'pytorch':
+    # the pytorch backend can be enabled by setting the VXM_BACKEND
+    # environment var to "pytorch"
+    from . import pytorch_backend
+    from .pytorch_backend import layers
+    from .pytorch_backend import networks
+    from .pytorch_backend import losses
 else:
     # tensorflow is default backend
-    from . import tf_backend as backend
-    from .tf_backend import layers
-    from .tf_backend import networks
-    from .tf_backend import losses
+    from . import tensorflow_backend
+    from .tensorflow_backend import layers
+    from .tensorflow_backend import networks
+    from .tensorflow_backend import losses
