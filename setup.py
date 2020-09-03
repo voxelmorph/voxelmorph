4cd892e2c5dcf77a5ea6c570df8f33efe9e9c706
Andrew Hoopes <athoopes1@gmail.com>
2020-09-02 22:56:47 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,25 @@ import setuptools
 
 setuptools.setup(
     name='voxelmorph',
-    version='1.0.0',
+    version='0.1',
+    license='gpl-3.0',
     description='Image Registration with Convolutional Networks',
     url='https://github.com/voxelmorph/voxelmorph',
+    keywords=['deformation', 'registration', 'imaging', 'cnn', 'mri'],
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     classifiers=[
+        'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
     install_requires=[
+        'scikit-image',
+        'h5py',
         'numpy',
         'scipy',
         'nibabel',
+        'neurite',
     ]
 )
