8e48fd11b515ba4fefe46aefc8a026e119acb540
Andrew Hoopes <athoopes1@gmail.com>
2020-09-11 22:40:19 -0400

    bf: dice loss now functional

--- pytorch/losses.py
+++ pytorch/losses.py
@@ -29,8 +29,10 @@ def mse_loss(x, y):
 
 
 def diceLoss(y_true, y_pred):
-    top = 2 * (y_true * y_pred, [1, 2, 3]).sum()
-    bottom = torch.max((y_true + y_pred, [1, 2, 3]).sum(), 50)
+    ndims = len(list(y_pred.size())) - 2
+    vol_axes = list(range(2, ndims+2))
+    top = 2 * (y_true * y_pred).sum(dim=vol_axes)
+    bottom = (y_true + y_pred).sum(dim=vol_axes)
     dice = torch.mean(top / bottom)
     return -dice
 
