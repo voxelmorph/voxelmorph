5d4879b711c663eb30dd90141342041eec9eea23
Andrew Hoopes <athoopes1@gmail.com>
2020-09-11 22:58:44 -0400

    bf: make sure torch dice has no div by zero

--- pytorch/losses.py
+++ pytorch/losses.py
@@ -32,7 +32,7 @@ def diceLoss(y_true, y_pred):
     ndims = len(list(y_pred.size())) - 2
     vol_axes = list(range(2, ndims+2))
     top = 2 * (y_true * y_pred).sum(dim=vol_axes)
-    bottom = (y_true + y_pred).sum(dim=vol_axes)
+    bottom = torch.clamp((y_true + y_pred).sum(dim=vol_axes), min=1e-5)
     dice = torch.mean(top / bottom)
     return -dice
 
