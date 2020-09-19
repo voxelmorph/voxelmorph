25be63c578c47c58b72315bd37b3e65c6e6f133a
Andrew Hoopes <athoopes1@gmail.com>
2020-09-19 09:05:14 -0400

    add checks for backend package

--- voxelmorph/__init__.py
+++ voxelmorph/__init__.py
@@ -8,15 +8,27 @@ from .py.utils import default_unet_features
 
 # import backend-dependent submodules
 backend = py.utils.get_backend()
+
 if backend == 'pytorch':
     # the pytorch backend can be enabled by setting the VXM_BACKEND
     # environment var to "pytorch"
+    try:
+        import torch
+    except ImportError:
+        raise ImportError('Please install pytorch to use this voxelmorph backend')
+
     from . import torch
     from .torch import layers
     from .torch import networks
     from .torch import losses
+
 else:
     # tensorflow is default backend
+    try:
+        import tensorflow
+    except ImportError:
+        raise ImportError('Please install tensorflow to use this voxelmorph backend')
+
     from . import tf
     from .tf import layers
     from .tf import networks
