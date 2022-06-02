54dc6902714d42e2bc028b940f3d632a17c43a6a
Andrew Hoopes <athoopes1@gmail.com>
2022-06-02 13:27:30 -0400

    add formal release versioning

--- voxelmorph/__init__.py
+++ voxelmorph/__init__.py
@@ -1,6 +1,22 @@
 # ---- voxelmorph ----
 # unsupervised learning for image registration
 
+
+# set version
+__version__ = '0.2'
+
+
+from packaging import version
+
+# ensure valid neurite version is available
+import neurite
+minv = '0.2'
+curv = getattr(neurite, '__version__', None)
+if curv is None or version.parse(curv) < version.parse(minv):
+    raise ImportError(f'voxelmorph requires neurite version {minv} or greater, '
+                      f'but found version {curv}')
+
+# move on the actual voxelmorph imports
 from . import generators
 from . import py
 from .py.utils import default_unet_features
@@ -28,6 +44,13 @@ else:
         import tensorflow
     except ImportError:
         raise ImportError('Please install tensorflow to use this voxelmorph backend')
+
+    # ensure valid tensorflow version is available
+    minv = '2.4'
+    curv = getattr(tensorflow, '__version__', None)
+    if curv is None or version.parse(curv) < version.parse(minv):
+        raise ImportError(f'voxelmorph requires tensorflow version {minv} or greater, '
+                          f'but found version {curv}')
 
     from . import tf
     from .tf import layers
